582a04686c8a8badf5036d066e4bf8873ed08a75
NicoArbogast <nicolas.arbogast@gmail.com>
2013-09-10 12:02:50 +0200

    more flexible authorized_keys

--- resources/authorized_keys.rb
+++ resources/authorized_keys.rb
@@ -3,6 +3,7 @@ actions :setup
 attribute :username,  :kind_of => String, :name_attribute => true
 attribute :ssh_keys,  :kind_of => Array, :default => []
 attribute :home,      :kind_of => String
+attribute :ssh_folder,:kind_of => String
 
 def initialize(*args)
   super
